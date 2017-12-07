fe1e630e9943bf51249722791a7ff93bf5351a5a
Czarny19 <czarnylukasz19@gmail.com>
2017-12-07 20:51:20 +0100

    dodanie swagger-ui

---
+++ src/main/java/Controller/UserController.java
@@ -0,0 +1,7 @@
+package Controller;
+
+/**
+ * Created by User on 2017-12-07.
+ */
+public class UserController {
+}
---
+++ src/main/java/com/cinema/SwaggerConfig.java
@@ -0,0 +1,7 @@
+package com.cinema;
+
+/**
+ * Created by User on 2017-12-07.
+ */
+public class SwaggerConfig {
+}
