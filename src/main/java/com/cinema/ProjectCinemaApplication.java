5d3bde2cf4e8efac936c22ee6e41f433700ae96a
Czarny19 <czarnylukasz19@gmail.com>
2017-12-07 15:08:27 +0100

    merge

---
+++ src/main/java/com/cinema/ProjectCinemaApplication.java
@@ -0,0 +1,12 @@
+package com.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCinemaApplication.class, args);
+	}
+}
