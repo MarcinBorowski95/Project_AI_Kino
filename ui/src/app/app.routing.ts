509d26f00aeceb1f1f70a978b9f5361c350d0a6d
Marcin <bomarc2@gmail.com>
2017-12-10 21:14:42 +0100

    BasicLayout

--- ui/src/app/app.routing.ts
+++ ui/src/app/app.routing.ts
@@ -7,7 +7,7 @@ import { RegisterComponent } from "./register/register.component";
 const routes: Routes = [
     {path: "" , loadChildren: 'app/home/home.module#HomeModule'} ,
     {path: "login" , loadChildren: 'app/login/login.module#LoginModule'} ,
-    {path: "register" , loadChildren: 'app/register/register.module#registerModule'},
+    {path: "register" , loadChildren: 'app/register/register.module#RegisterModule'},
     {path: "**" , redirectTo: "/" }
     
   ];
