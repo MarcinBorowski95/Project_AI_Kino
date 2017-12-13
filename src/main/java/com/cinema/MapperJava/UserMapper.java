6ebaa58811f4be311770fe087790c32307c5aad8
JeBudSu <mariuszbyler@gmail.com>
2017-12-14 00:18:27 +0100

    dupa

---
+++ src/main/java/com/cinema/MapperJava/UserMapper.java
@@ -0,0 +1,13 @@
+package com.cinema.MapperJava;
+
+import com.cinema.Domain.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    User findByEmail(@Param("userName") String userName);
+    List<User> getUsers(@Param("e_mail") String e_mail);
+}
