d1d159380bd11a25ce4c488fe0e99b3972b0fc98
Marcin <bomarc2@gmail.com>
2017-12-14 20:11:34 +0100

    testing

--- ui/src/app/repertoire/repertoire.component.ts
+++ ui/src/app/repertoire/repertoire.component.ts
@@ -2,6 +2,7 @@ import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import { Component, OnInit } from '@angular/core';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-repertoire',
@@ -11,9 +12,11 @@ import { Component, OnInit } from '@angular/core';
 export class RepertoireComponent implements OnInit {
   
   private movies: any[];
+  private hours: any[];
   private day: String = "1";
-  private movieUrl = 'http://localhost:4200/api/movie?day=' + this.day;
-  private seansUrl = 'http://localhost:4200/api/showtime?id_movie=' + "1" + '&day=' + this.day;
+  private movieUrl = 'http://localhost:4200/api/movies?day=' + this.day;
+  private seanseUrl
+  
 
   constructor(private http: Http) { }
   
@@ -25,6 +28,14 @@ export class RepertoireComponent implements OnInit {
     return this.http.get(this.movieUrl).subscribe(res => {
       this.movies = res.json();
       console.log(this.movies);
+    });
+  }
+
+  getSeans(movieID: any): any {
+    this.seanseUrl = 'http://localhost:4200/api/showtime?id_movie=' + movieID + '&day=' + this.day
+    return this.http.get(this.seanseUrl).subscribe(res => {
+      this.hours = res.json();
+      console.log(this.hours);
     });
   }
 }
