c3090d17f72a3bfc9f4034affcef86e167c32746
Marcin <bomarc2@gmail.com>
2017-12-14 23:55:26 +0100

    sala

---
+++ ui/src/app/sala/sala.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SalaComponent } from './sala.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SalaComponent]
+})
+export class SalaModule { }
