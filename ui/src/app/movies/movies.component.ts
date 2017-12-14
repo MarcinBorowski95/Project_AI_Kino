aca09ee0dc8797a7daa560279db0d9f58355f917
Marcin <bomarc2@gmail.com>
2017-12-14 17:50:00 +0100

    RepMovieDBngFor

--- ui/src/app/movies/movies.component.ts
+++ ui/src/app/movies/movies.component.ts
@@ -11,12 +11,12 @@ import 'rxjs/add/operator/map';
 })
 export class MoviesComponent implements OnInit {
 
-  private items: any[];
+  private movies: any[];
 
   constructor(private http: Http) { }
 
   // private instance variable to hold base url
-  private movieUrl = 'http://localhost:4200/api/movie?day=1';
+  private movieUrl = 'http://localhost:4200/api/movies';
 
   ngOnInit() {
     this.getMovie();
@@ -24,8 +24,8 @@ export class MoviesComponent implements OnInit {
 
   getMovie(): any {
     return this.http.get(this.movieUrl).subscribe(res => {
-      this.items = res.json();
-      console.log(this.items);
+      this.movies = res.json();
+      console.log(this.movies);
     });
   }
 }
--- ui/src/app/repertoire/repertoire.component.ts
+++ ui/src/app/repertoire/repertoire.component.ts
@@ -1,3 +1,6 @@
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repertoire.component.css']
 })
 export class RepertoireComponent implements OnInit {
+  
+  private movies: any[];
+  private day: String = "1";
+  private movieUrl = 'http://localhost:4200/api/movie?day=' + this.day;
+  private seansUrl = 'http://localhost:4200/api/showtime?id_movie=' + "1" + '&day=' + this.day;
 
-  constructor() { }
-
+  constructor(private http: Http) { }
+  
   ngOnInit() {
+    this.getMovie();
   }
 
+  getMovie(): any {
+    return this.http.get(this.movieUrl).subscribe(res => {
+      this.movies = res.json();
+      console.log(this.movies);
+    });
+  }
 }
