5f03a3b158b157ded74d58dc40cb06a3edcc57f8
Marcin <bomarc2@gmail.com>
2017-12-16 17:39:26 +0100

    salaV2

--- ui/src/app/sala/sala.component.ts
+++ ui/src/app/sala/sala.component.ts
@@ -14,6 +14,7 @@ export class SalaComponent implements OnInit {
   rows;
   seats;
   private roomUrl;
+  private seatUrl;
   seans;
 
   constructor(
@@ -25,6 +26,7 @@ export class SalaComponent implements OnInit {
   ngOnInit() {
     this.data.currentMessage.subscribe(seans => this.seans = seans)
     this.getRoom();
+    this.getSeats();
   }
 
   getRoom(): any {
@@ -32,9 +34,16 @@ export class SalaComponent implements OnInit {
     return this.http.get(this.roomUrl).subscribe(res => {
       this.room = res.json();
       console.log(this.room);
-      this.rows = Array(this.room.rows).fill(1);
+      this.rows = [{number: "1"}, {number: "2"}, {number: "3"}];
       console.log(this.rows)
-      this.seats= Array(this.room.seatsInRow).fill(1);
+    });
+  }
+
+  getSeats(): any {
+    this.seatUrl = 'http://localhost:4200/api/getSeatsByIdShowtime?id_showtime=' + this.seans;
+    return this.http.get(this.seatUrl).subscribe(res => {
+      this.seats = res.json();
+      console.log(this.seats);
     });
   }
 
