de7e07bb7c29d43a0bf180b725f02d42f6565183
Marcin <bomarc2@gmail.com>
2017-12-23 15:25:50 +0100

    WorkingCarouselle

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { Routing } from './app.routing';
@@ -14,7 +15,8 @@ import { DataService } from './_services/data.service';
   imports: [
     BrowserModule,
     Routing,
-    FormsModule
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
--- ui/src/app/home/home.module.ts
+++ ui/src/app/home/home.module.ts
@@ -1,3 +1,4 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -10,7 +11,8 @@ const routes = [
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    NgbModule
   ],
   declarations: [HomeComponent]
 })
