2b9594251f742a5e2d21ee926c4e7502c6e8c7d6
Marcin <bomarc2@gmail.com>
2017-12-26 12:31:24 +0100

    BuyTicketPoprawioneKaruzelaPoprawiona

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [NgbCarouselConfig],
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbCarouselConfig) {
+    config.wrap = true;
+   }
 
   ngOnInit() {
   }
