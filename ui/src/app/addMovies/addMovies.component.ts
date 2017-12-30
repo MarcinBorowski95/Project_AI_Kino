efc6bbc499fb85a31b8b7fb2453a8d88450240a4
Marcin <bomarc2@gmail.com>
2017-12-30 17:04:56 +0100

    AddMoviesV2

--- ui/src/app/addMovies/addMovies.component.ts
+++ ui/src/app/addMovies/addMovies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-addMovies',
@@ -11,12 +12,40 @@ export class AddMoviesComponent implements OnInit {
   };
 
   generes = [
-    "horror", "komedia", "thriller", "film akcji", "film romantyczny", "film animowany", 
+    "horror", "komedia", "thriller", "film akcji", "film romantyczny", "film animowany",
   ]
 
-  constructor() { }
+  private movies: any[];
+  private movieUrl = 'http://localhost:4200/api/getAllCurrentMovies';
+
+  constructor(private http: Http) { }
 
   ngOnInit() {
+    this.getMovie();
+  }
+
+  getMovie(): any {
+    return this.http.get(this.movieUrl).subscribe(res => {
+      this.movies = res.json();
+      console.log(this.movies);
+    });
+  }
+  addMovie(valid) {
+    if (valid) {
+      if (this.movies.forEach(movieDB => {
+        if (movieDB.title == this.movie.title || movieDB.title_pl == this.movie.titlePL) {
+          return true;
+        }
+      })) {
+        alert("taki film istnieje")
+      } else {
+        alert("Poprawnie wypełnione")
+        console.log("Do uzupełnienia");
+        
+      }
+    } else {
+      alert("Błędnie wypełniona")
+    }
   }
 
 }
