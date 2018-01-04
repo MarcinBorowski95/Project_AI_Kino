c76325bbf2f5845c7ba0a42b33a8baca4f94bce3
Marcin <bomarc2@gmail.com>
2018-01-05 00:03:25 +0100

    routing

--- ui/src/app/checkTicket/checkTicket.module.ts
+++ ui/src/app/checkTicket/checkTicket.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckTicketComponent } from './checkTicket.component';
+import { RouterModule } from '@angular/router';
+
+const routes = [
+  { path: '', component: CheckTicketComponent }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
   ],
   declarations: [CheckTicketComponent]
 })
