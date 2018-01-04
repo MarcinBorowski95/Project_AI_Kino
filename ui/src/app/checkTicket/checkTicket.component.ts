d2a2695214023bdbb874c887062df98516f0778c
Marcin <bomarc2@gmail.com>
2018-01-04 23:49:43 +0100

    checkTicket

---
+++ ui/src/app/checkTicket/checkTicket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkTicket',
+  templateUrl: './checkTicket.component.html',
+  styleUrls: ['./checkTicket.component.css']
+})
+export class CheckTicketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ ui/src/app/checkTicket/checkTicket.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CheckTicketComponent } from './checkTicket.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CheckTicketComponent]
+})
+export class CheckTicketModule { }
